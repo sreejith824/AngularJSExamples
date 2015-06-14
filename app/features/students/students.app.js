3f13d3fb276e044153a8cec4ebaf4a1c178d4d61
sreejith824 <KhulaDevVM@metropolitan.co.za>
2015-06-14 19:51:26 +0200

    Students module : LTPA token implementation

---
+++ app/features/students/students.app.js
@@ -0,0 +1,15 @@
+(function () {
+  'use strict';
+
+  /**
+   * @ngdoc overview
+   * @name maintainClient
+   * @description
+   * # maintainClient
+   *
+   * Main module of the application.
+   */
+  angular.module('students.app',
+    ['ui.router','ngCookies']);
+
+})();
